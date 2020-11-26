ccdd6ce991c172f05c76abdaf3958a1d140984ba
DaDa0413 <actf5795nb8@gmail.com>
2020-11-26 13:21:09 +0800

    Modify shutdown.py exception and comment rfrespond

--- code/shutdown.py
+++ code/shutdown.py
@@ -6,29 +6,28 @@ import subprocess
 if not os.geteuid()==0:
     sys.exit("Hint: call me as root")
 
-process = subprocess.Popen(['/usr/local/bin/rfrespond'])
-try:
-    print('[INFO] rfespond: ', process.pid)
-    process.wait(timeout=15)
-except subprocess.TimeoutExpired:
-    print('Timed out - killing rfrespond')
-    process.kill()
+# process = subprocess.Popen(['/usr/local/bin/rfrespond'])
+# try:
+#     print('[INFO] rfespond: ', process.pid)
+#     process.wait(timeout=15)
+# except subprocess.TimeoutExpired:
+#     print('Timed out - killing rfrespond')
+#     process.kill()
 
-# time.sleep(15) # Wait for "rfresponse" send ACK
 process = subprocess.Popen(['/usr/local/bin/cleanFIFO'])
 try:
     print('[INFO] cleanFIFO: ', process.pid)
     process.wait()
-except:
-    print "cleanFIFO Unexpected error:", sys.exc_info()
+except subprocess.CalledProcessError as e:
+    print "cleanFIFO Unexpected error:", e.output
     process.kill()
     
 process = subprocess.Popen(['/usr/local/bin/rfwait'])
 try:
     print('[INFO] rfwait: ', process.pid)
     process.wait()
-except:
-    print "Rfwait Unexpected error:", sys.exc_info()
+except subprocess.CalledProcessError as e:
+    print "Rfwait Unexpected error:", e.output
     process.kill()
 
 print("Bye!\n")
