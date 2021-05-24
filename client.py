2eed9b46896817f65f19052ff3d4c4dbe4ffede9
DaDa0413 <actf5795nb8@gmail.com>
2021-05-24 13:22:23 +0800

    Add udp server and client

---
+++ client.py
@@ -0,0 +1,11 @@
+#Send UDP broadcast packets
+MYPORT = 50000
+import sys, time
+from socket import *
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(('', 0))
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+while 1:
+    data = repr(0)
+    s.sendto(data, ('192.168.10.2', MYPORT))
+    time.sleep(2)
